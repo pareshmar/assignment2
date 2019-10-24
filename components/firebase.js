8c55517f534bf73ff70f603b1b566478d04d6789
Pareshma Rampersaud <pareshmarampersaud@gmail.com>
2019-10-24 12:10:04 -0400

    first commit

--- components/firebase.js
+++ components/firebase.js
@@ -2,13 +2,9 @@ import * as firebase from 'firebase';
 
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyAqwMcKSqGkxf98XUfK2lDtxCr4V5UUMi8",
     authDomain: "assignment2-firebase1.firebaseapp.com",
     databaseURL: "https://assignment2-firebase1.firebaseio.com",
-    projectId: "assignment2-firebase1",
     storageBucket: "assignment2-firebase1.appspot.com",
-    messagingSenderId: "2312350713",
-    appId: "1:2312350713:web:854ad9087d4f15f421eb28"
 };
 
 firebase.initializeApp(firebaseConfig);
